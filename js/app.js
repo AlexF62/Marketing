0d54f251e40624b3975401a93c8a0adf5ea70a1b
Alex <105883808+AlexF62@users.noreply.github.com>
2023-01-06 11:23:37 +0300

    final project

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(function () {
+
+  $("#menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({
+      scrollTop: top
+    }, 1500);
+  });
+
+
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+
+    prevArrow: '<button type="button" class="slick-prev"><img src="/image/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="/image/arrow-right.svg" alt=""></button>',
+    responsive: [{
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+
+        }
+      },
+
+
+    ]
+  });
+
+  $(`.menu__btn`).on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  var mixer = mixitup('.portfolio__content');
+
+
+
+
+
+
+
+
+});
